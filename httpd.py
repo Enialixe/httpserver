7c5a6aae8924e00917c06746929f1fd51426a296
Enialixe <aleksedme@gmail.com>
2019-04-13 21:49:17 +0300

    error fix

--- httpd.py
+++ httpd.py
@@ -8,25 +8,37 @@ import os
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 
-def quitServer(sig, unused):
+def quit_server(sig, unused):
     server.shutdown()
     sys.exit(1)
 
-def init_logger():
+
+def init_logger(log_level):
     log_path = os.path.join(script_dir, 'server.log')
-    logging.basicConfig(filename=log_path, level=logging.ERROR,
+    if log_level == 'error':
+        log_level = logging.ERROR
+    elif log_level == 'debug':
+        log_level = logging.DEBUG
+    elif log_level == 'info':
+        log_level = logging.INFO
+    logging.basicConfig(filename=log_path, level=log_level,
                         format='[%(asctime)s] %(levelname).1s %(message)s',
                         datefmt='%Y.%m.%d %H:%M:%S')
     logging.info('Loggin is started')
 
+
 if __name__ == "__main__":
-    signal.signal(signal.SIGINT, quitServer)
-    init_logger()
+    signal.signal(signal.SIGINT, quit_server)
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', type=int)
     parser.add_argument('--document_root')
     parser.add_argument('--workers', type=int)
+    parser.add_argument('--host')
+    parser.add_argument('--log_level')
     args = parser.parse_args()
-    server = server.HTTPServer(port=args.port, document_root=args.document_root, workers=args.workers)
-    server.star()
-    print('HTTP server started')
+    if not args.log_level:
+        init_logger('error')
+    else:
+        init_logger(args.log_level)
+    server = server.HTTPServer(host=args.host, port=args.port, document_root=args.document_root, workers=args.workers)
+    server.start()
--- server.py
+++ server.py
@@ -7,28 +7,43 @@ import os
 import datetime
 import urllib
 import traceback
+import mimetypes
+import time
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 class HTTPServer(object):
 
-    def __init__(self, port=8080, document_root=script_dir, workers=10):
-        self.host = socket.gethostname().split()[0]
-        self.port = port
-        logging.info('Root directory {document_root}'.format(document_root=document_root))
-        self.root_dir = document_root
+    def __init__(self, host=None, port=None, document_root=None, workers=None, thread_timeout = 1):
+        if host:
+            self.host = host
+        else:
+            self.host = socket.gethostname().split()[0]
+        if port:
+            self.port = port
+        else:
+            self.port = 8080
+        if document_root:
+            self.root_dir = document_root
+        else:
+            self.root_dir = script_dir
+        if workers:
+            self.workers = workers
+        else:
+            self.workers = 2
+        self.mime_type = mimetypes.MimeTypes()
+        logging.info('Root directory {document_root}'.format(document_root=self.root_dir))
         self.queue = Queue.Queue()
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        for i in range(workers):
+        self.thread_timeout = thread_timeout
+        for i in range(self.workers):
             thread = threading.Thread(target=self.__proceed_sessions, args=(self.queue,))
             thread.daemon = True
-            thread.name=str(i)
+            thread.name = str(i)
             thread.start()
-        logging.info('Started {workers} workers'.format(workers=workers))
-
-    def start(self):
+        logging.info('Started {workers} workers'.format(workers=self.workers))
         try:
             logging.info('Startin HTTP-server on {host}:{port}'.format(host=self.host, port=self.port))
             self.socket.bind((self.host, self.port))
@@ -36,20 +51,11 @@ class HTTPServer(object):
             logging.error('Can not bind to {port}'.format(port=self.port))
             self.shutdown()
             sys.exit(1)
-        self._receive_connections()
 
-    def shutdown(self):
-        try:
-            logging.info('Stoping http server')
-            self.socket.close()
-        except:
-            logging.error('Socket is already down')
-
-
-
-
-    def _receive_connections(self):
+    def start(self):
         self.socket.listen(5)
+        print('HTTP server started')
+        logging.info('HTTP server started')
         while True:
             (client, address) = self.socket.accept()
             if not client.gettimeout():
@@ -57,26 +63,47 @@ class HTTPServer(object):
             logging.info('Recieved connction from {address}'.format(address=address))
             self.queue.put((client, address))
 
+    def shutdown(self):
+        try:
+            for i in range(self.workers):
+                self.queue.put(('quit', 'quit'))
+            logging.info('Stoping http server')
+            self.socket.close()
+        except:
+            logging.error('Socket is already down')
+
     def __proceed_sessions(self, queue):
         packet_size = 1024
         while True:
             try:
-                (client, address) = queue.get()
-
-                data = client.recv(packet_size)
-                logging.debug('Worker {thread} get job {data}'.format(thread=threading.current_thread().name,
+                (client, address) = queue.get_nowait()
+                if client == 'quit' and address == 'quit':
+                    self.queue.task_done()
+                    break
+                else:
+                    buffer = ''
+                    while (len(buffer)<4096):
+                        data = client.recv(packet_size)
+                        logging.debug('Worker {thread} get job {data}'.format(thread=threading.current_thread().name,
                                                                                    data = data))
-                if not data or ('\r\n' not in data and ' ' not in data): continue
-                response = self.__make_response(data)
-                client.send(response)
-                client.close()
+                        buffer += data
+                        if buffer or ('\r\n' in buffer and ' ' in buffer):
+                            logging.debug(buffer)
+                            break
+                    response = self.__make_response(buffer)
+                    client.sendall(response)
+                    client.close()
+                    self.queue.task_done()
             except Queue.Empty:
                 pass
-            except Exception as E:
+            except Exception:
                 logging.error(traceback.format_exc())
+            finally:
+                time.sleep(self.thread_timeout/1000000.0)
+        logging.info('Worker {thread} quit'.format(thread=threading.current_thread().name))
 
-    @staticmethod
-    def __generate_headers(response_code, file_size, file_type):
+
+    def __generate_headers(self, response_code, file_path = None):
         headers = ''
         if response_code == 200:
             headers += 'HTTP/1.1 200 OK\r\n'
@@ -86,69 +113,67 @@ class HTTPServer(object):
             headers += 'HTTP/1.1 405 Method Not Allowed\r\n'
         elif response_code == 403:
             headers += 'HTTP/1.1 403 Forbidden\r\n'
+        logging.debug('Responce with code {code}'.format(code=response_code))
         date = datetime.datetime.now().strftime("%a, %d %b %Y %H:%M:%S")
         headers += 'Date: {now}\n'.format(now=date)
         headers += 'Server: Otus-HTTP-server\r\n'
-        if file_size:
-            headers += 'Content-Length: {size}\r\n'.format(size=file_size)
-            if file_type in ['.html', '.css']:
-                content_type = 'text/{type}'.format(type=file_type[1:])
-            elif file_type == '.js':
-                content_type = 'application/javascript'
-            elif file_type == '.swf':
-                content_type = 'application/x-shockwave-flash'
-            elif file_type in ['.jpg', '.jpeg', '.png', '.gif', '.swf']:
-                if file_type == '.jpg':
-                    file_type = '.jpeg'
-                content_type = 'image/{type}'.format(type=file_type[1:])
-            else:
-                content_type = None
-            if content_type:
-                headers += 'Content-Type: {content}\r\n'.format(content=content_type)
+        if response_code == 200 and file_path:
+            headers += 'Content-Length: {size}\r\n'.format(size=os.path.getsize(file_path))
+            content_type = self.mime_type.guess_type(os.path.basename(file_path))[0]
+            logging.debug('Responce content type {content}'.format(content=content_type))
+            headers += 'Content-Type: {content}\r\n'.format(content=content_type)
+        logging.debug('Headers {headers}'.format(headers=headers))
         return headers
 
     def __make_response(self, data):
-        (response_code, file_bytes, file_size, file_type) = self._parse_data(data.decode())
-        response = self.__generate_headers(response_code, file_size, file_type).encode()
+        request_method, file_path = self._parse_data(data.decode())
+        if not request_method:
+            response_code = 405
+        elif not file_path:
+            response_code = 404
+        elif file_path == 'forbidden':
+            response_code = 403
+        else:
+            response_code = 200
+        if file_path == 'forbidden':
+            file_path = None
+        response = self.__generate_headers(response_code, file_path).encode()
         response += '\r\n'
-        if file_bytes:
-            response += file_bytes
+        if request_method != 'HEAD' and response_code == 200:
+            with open(file_path, 'rb') as f:
+                response += f.read()
         return response
+
+    def _normalize_path(self, file_path):
+        if file_path.find(self.root_dir) == -1:
+            if file_path.startswith('/'):
+                file_path = file_path[1:]
+            file_path = os.path.join(self.root_dir, file_path)
+            logging.debug('New file path is {file_path}'.format(file_path=file_path))
+        file_path = urllib.unquote(file_path)
+        if os.path.isdir(file_path):
+            file_path = os.path.join(file_path, 'index.html')
+            logging.debug('Index file path is {file_path}'.format(file_path=file_path))
+        elif '?' in file_path:
+            file_path = file_path.split('?')[0]
+        if '/../' in file_path:
+            file_path = 'forbidden'
+        if not os.path.isfile(file_path):
+            file_path = None
+        return file_path
 
     def _parse_data(self, data):
         logging.debug('{data}'.format(data=data.split('\r\n')))
         request_method = data.split('\r\n')[0].split(' ')[0]
         try:
             file_path = data.split('\r\n')[0].split(' ')[1]
-        except Exception as E:
+        except Exception:
             logging.error('Wrong file path in {data}'.format(data=data.split('\r\n')[0]))
-            raise E
+            raise Exception
         logging.debug('Request_method {request_method} file path {file_path}'.format(request_method=request_method,
-                                                                                    file_path=file_path))
-        if request_method in ('GET', 'HEAD'):
-            if file_path.find(self.root_dir) == -1:
-                file_path =self.root_dir + file_path
-                logging.debug('New file path is {file_path}'.format(file_path=file_path))
-            file_path = urllib.unquote(file_path)
-            if (os.path.isdir(file_path)):
-                logging.debug('Index file path is {file_path}'.format(file_path=file_path))
-                file_path = file_path+'index.html'
-            elif ('?' in file_path):
-                file_path = file_path.split('?')[0]
-            if '/../' in file_path:
-                return 403, None, 0, None
-            if (not os.path.isfile(file_path)):
-                logging.info('Client is trying to get file that not exitst {file_path}'.format(file_path=file_path))
-                return 404, None, 0, None
-            with open(file_path, 'rb') as file:
-                file_bytes = file.read()
-            file_size = os.path.getsize(file_path)
-            if request_method == 'HEAD':
-                logging.info('Request method is HEAD')
-                return 200, None, file_size, os.path.splitext(file_path)[1]
-            else:
-                logging.info('Request method is GET')
-                return 200, file_bytes, file_size, os.path.splitext(file_path)[1]
-        else:
-            return 405, '', 0, ''
-
+                                                                                     file_path=file_path))
+        file_path = self._normalize_path(file_path)
+        logging.debug('Result file path is {file_path}'.format(file_path=file_path))
+        if request_method not in ('GET', 'HEAD'):
+            request_method = None
+        return request_method, file_path
